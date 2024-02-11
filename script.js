bdd2900af79e7b298cbae7230e06d0a40372e22b
imLuiszzz <bygusland@gmail.com>
2024-02-11 06:13:19 -0500

    a

--- script.js
+++ script.js
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "Estas segura?",
+    "Para pss",
+    "No me hagas esto :(",
+    "Estás rompiendo mi corazón",
+    "voy a llorar...",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
